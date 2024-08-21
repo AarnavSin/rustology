5c3f1826827ed86a471ad98e1ae7743f66be5813
Aarnav S <62804454+AarnavSin@users.noreply.github.com>
2024-08-21 08:55:23 +0530

    Remove unneeded mut variabl;e

--- convert_temp/src/main.rs
+++ convert_temp/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let mut temp_celsius: f64 = 32.0;
+    let temp_celsius: f64 = 32.0;
     let temp_fahrenheit: f64 = ((temp_celsius * 1.8) + 32.0);
     println!("The temparature in Celsius is {temp_celsius}°C.\nThe temperature in Fahrenheit is {temp_fahrenheit}°F.");
 }
